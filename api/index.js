7b6cdcfe0c6dcf956df798e67a978446670a91cd
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2020-12-11 16:20:34 -0300

    Add api docs with swagger

--- api/index.js
+++ api/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const parser = require('body-parser');
 
+const swaggerUI = require('swagger-ui-express');
+
 const config = require('../config');
 const user = require('./components/user/network');
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json());
+app.use(parser.json());
+
+const swaggerDoc = require('./swagger.json');
 
 // Router
 
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
     console.log(`Api listening in port: ${config.api.port}`);
